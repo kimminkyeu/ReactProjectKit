5ec10983341d3c36b8a3d967b0c5f322438642ab
kimminkyeu <minkyeki@student.42seoul.kr>
2023-02-09 23:54:50 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Counter } from '@/components/Counter';
+
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Counter message="hello" />
+        </header>
+      </div>
+    );
+  }
+export default App;
